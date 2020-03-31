9a898d52fa6214121f00ab0edf7d1e4d7bcfb484
西田俊陽 <toshi@toshinoMacBook-ea.local>
2020-03-31 16:06:44 +0900

    Add birth method
    誕生日を日本語に変換

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum status: [:men,:lady]
+
+  def birth #年月日に変換
+   self.birth_day.strftime("%Y年 %m月 %d日")
+  end
 end
