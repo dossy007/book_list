dd8027ca0f395a30283114f1e582f92a534b46ba
dossy <fedox.toshi@gmail.com>
2020-04-01 12:11:42 +0900

    Add routing book/end_book

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "books#index"
-  resources :books, only: [:index,:show]
+  resources :books, only: [:index,:show] do
+    collection do
+     post "end_book"
+    end
+  end
   resources :users, only: [:show]
 end
