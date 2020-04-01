d69605fafd7b561cdc0eca5b52ed2e9f09fa8955
dossy <fedox.toshi@gmail.com>
2020-04-01 12:12:39 +0900

    Add book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,10 @@ class BooksController < ApplicationController
   def index;@books = Book.limit(5);end
 
   def show;@book = Book.find(params[:id]);end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:book_id).merge(user_id: current_user.id)
+  end
 end
