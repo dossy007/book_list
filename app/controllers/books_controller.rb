8d694777fc14bf4f1cf6e0e64c90183dc26b3691
dossy <fedox.toshi@gmail.com>
2020-04-01 12:12:15 +0900

    Add before_action authenticate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user! ,only: [:end_book]
   def index;@books = Book.limit(5);end
 
   def show;@book = Book.find(params[:id]);end
