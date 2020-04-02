5c997f3b3ccbc3bb2ad30eaf9910686262c1e151
dossy <fedox.toshi@gmail.com>
2020-04-02 23:10:18 +0900

    Add book model method(function) reader &readover?

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,15 @@ class Book < ApplicationRecord
   
   has_many :users, through: :end_books
 
+  ##function
+  def readover(user)
+    end_books.create(user_id: user.id)
+  end
+
+
+  def readover?(user)
+    self.users.include?(user)
+  end
+
+
 end
