b8244f01d3d082b71e8379e026c6f95d20b32d45
dossy <fedox.toshi@gmail.com>
2020-04-02 23:08:43 +0900

    Add destroy end_books

--- app/controllers/end_books_controller.rb
+++ app/controllers/end_books_controller.rb
@@ -1,3 +1,5 @@
+class EndBooksController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @readover = Book.find(get_book)
@@ -11,3 +13,19 @@
         end
       end
   end
+
+  def destroy
+    e = EndBook.find(params[:id])
+    e.destroy
+    respond_to do |f|
+      f.html {redirect_to request.referrer || root_url}
+      f.js
+    end
+  end
+end
+
+private
+
+def get_book
+  params.require(:end_book)[:book_id]
+end
