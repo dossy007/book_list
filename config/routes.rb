e954904cc786ad74973079ac6c60866809778b56
dossy <fedox.toshi@gmail.com>
2020-04-02 21:44:37 +0900

    Fix routes create,destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "books#index"
   resources :books, only: [:index,:show] do
-    collection do
-     post "end_book"
-    end
   end
+  resources :end_books, only: [:create,:destroy]
   resources :users, only: [:show]
 end
