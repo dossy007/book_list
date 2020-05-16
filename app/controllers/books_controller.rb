d21012bbb80ef29d9c2aa7dc58a9b1834635d94b
dossy <fedox.toshi@gmail.com>
2020-05-16 20:54:42 +0900

    add subbooks

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
-  def index;@books = Book.limit(4);end
+  def index
+    @books = Book.limit(4)
+    @subbooks = Book.where("id > ? and id < ?", 4, 8) ##4超過8未満
+  end
 
   def show;@book = Book.find(params[:id]);end
 end
